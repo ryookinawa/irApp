e68e13c501d76c4402f76e1f415ba8bcbbe17e09
ryookinawa <ryookinawa@gmail.com>
2016-03-16 11:37:56 +0900

    commit

--- client/app/ir/ir.controller.js
+++ client/app/ir/ir.controller.js
@@ -1,10 +1,25 @@
 'use strict';
 
-angular.module('irAppApp')
-  .controller('IrCtrl', function ($scope, $http) {
-    $scope.irs = [];
+(function() {
 
-    $http.get('/api/irs').success(function(irs) {
-      $scope.irs = irs;
+class IrController {
+
+  constructor($http, $scope, socket) {
+    this.$http = $http;
+    this.awesomeThings = [];
+
+    $http.get('/api/irs').then(response => {
+      this.awesomeThings = response.data;
+      console.log(response.data);
+      socket.syncUpdates('thing', this.awesomeThings);
     });
-  });
+
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('thing');
+    });
+  }
+}
+
+angular.module('irAppApp')
+  .controller('IrCtrl', IrController);
+})();
